f546bc52d45d6c0fd232792e5e5bbee21840de7a
nolash <dev@holbrook.no>
2021-02-11 10:14:28 +0100

    Add progress callback and new block/tx objects from chainlib

--- chainsyncer/driver.py
+++ chainsyncer/driver.py
@@ -6,15 +6,21 @@ import time
 logg = logging.getLogger()
 
 
+def noop_progress(s):
+    logg.debug(s)
+
+
 class Syncer:
 
     running_global = True
+    yield_delay=0.005
 
-    def __init__(self, backend):
+    def __init__(self, backend, progress_callback=noop_progress):
         self.cursor = None
         self.running = True
         self.backend = backend
         self.filter = []
+        self.progress_callback = progress_callback
 
 
     def chain(self):
@@ -32,8 +38,8 @@ class Syncer:
 
 class MinedSyncer(Syncer):
 
-    def __init__(self, backend):
-        super(MinedSyncer, self).__init__(backend)
+    def __init__(self, backend, progress_callback):
+        super(MinedSyncer, self).__init__(backend, progress_callback)
 
 
     def loop(self, interval, getter):
@@ -43,30 +49,31 @@ class MinedSyncer(Syncer):
                 if block_hash == None:
                     break
                 self.process(getter, block_hash)
+                time.sleep(self.yield_delay)
             time.sleep(interval)
 
 
 class HeadSyncer(MinedSyncer):
 
-    def __init__(self, backend):
-        super(HeadSyncer, self).__init__(backend)
+    def __init__(self, backend, progress_callback):
+        super(HeadSyncer, self).__init__(backend, progress_callback)
 
 
     def process(self, getter, block):
         logg.debug('process {}'.format(block))
-        block = getter.block_by_hash(block.hash)
         i = 0
         tx = None
         while True:
             try:
                 #self.filter[0].handle(getter, block, None)
                 tx = block.tx(i)
-                logg.debug('tx {}'.format(tx))
-                self.backend.set(block.number(), i)
+                self.progress_callback('processing {}'.format(tx))
+                self.backend.set(block.number, i)
                 for f in self.filter:
-                    f(getter, block, tx)
+                    f.handle(getter, block, tx)
+                    self.progress_callback('applied filter {} on {}'.format(f.name(), tx))
             except IndexError as e:
-                self.backend.set(block.number() + 1, 0)
+                self.backend.set(block.number + 1, 0)
                 break
             i += 1
         
@@ -75,7 +82,7 @@ class HeadSyncer(MinedSyncer):
         (block_number, tx_number) = self.backend.get()
         block_hash = []
         uu = uuid.uuid4()
-        res = getter.block_by_integer(block_number)
+        res = getter.get(block_number)
         logg.debug('get {}'.format(res))
 
         return res
