af6eedf87e29e4c5c59586a6dcc3bce1c015405e
lash <dev@holbrook.no>
2022-03-30 08:13:34 +0000

    Set defaults for common name and sum in filter

--- chainsyncer/filter.py
+++ chainsyncer/filter.py
@@ -1,12 +1,20 @@
+# standard imports
+import hashlib
+
+
 class SyncFilter:
 
-    def common_name(self):
-        raise NotImplementedError()
-
-
     def sum(self):
-        raise NotImplementedError()
-
+        s = self.common_name()
+        h = hashlib.sha256()
+        h.update(s.encode('utf-8'))
+        return h.digest()
+        
 
     def filter(self, conn, block, tx):
         raise NotImplementedError()
+
+
+    def common_name(self):
+        s = self.__module__ + '.' + self.__class__.__name__
+        return s.replace('.', '_')
