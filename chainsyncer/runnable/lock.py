36acf3f09ac71d7a5e97abe25e2dec631e064e59
lash <dev@holbrook.no>
2022-04-27 09:59:40 +0000

    WIP more work on lock cli tool

--- chainsyncer/runnable/lock.py
+++ chainsyncer/runnable/lock.py
@@ -8,10 +8,12 @@ import importlib
 
 # external imports
 import chainlib.cli
+from shep.persist import PersistedState
 
 # local imports
 import chainsyncer.cli
 from chainsyncer.settings import ChainsyncerSettings
+from chainsyncer.filter import FilterState
 
 logging.basicConfig(level=logging.WARNING)
 logg = logging.getLogger()
@@ -20,9 +22,7 @@ logg = logging.getLogger()
 arg_flags = chainlib.cli.argflag_std_base | chainlib.cli.Flag.CHAIN_SPEC
 argparser = chainlib.cli.ArgumentParser(arg_flags)
 argparser.add_argument('--state-dir', type=str, dest='state_dir', help='State directory')
-argparser.add_argument('--session-id', type=str, dest='session_id', help='Session id for state')
-argparser.add_argument('--action', type=str, choices=['repeat', 'continue'], help='Action to take on lock. Repeat means re-run the locked filter. Continue means resume execution for next filter.')
-argparser.add_positional('tx', type=str, help='Transaction hash to unlock')
+argparser.add_positional('action', type=str, help='Action to take on lock. Repeat means re-run the locked filter. Continue means resume execution for next filter.')
 
 sync_flags = chainsyncer.cli.SyncFlag.RANGE | chainsyncer.cli.SyncFlag.HEAD
 chainsyncer.cli.process_flags(argparser, sync_flags)
@@ -33,7 +33,6 @@ base_config_dir = chainsyncer.cli.config_dir,
 config = chainlib.cli.Config.from_args(args, arg_flags, base_config_dir=base_config_dir)
 config = chainsyncer.cli.process_config(config, args, sync_flags)
 config.add(args.state_dir, '_STATE_DIR', False)
-config.add(args.session_id, '_SESSION_ID', False)
 logg.debug('config loaded:\n{}'.format(config))
 
 settings = ChainsyncerSettings()
@@ -41,26 +40,24 @@ settings.process_sync_backend(config)
 
 
 def main():
-    state_dir = None
     if settings.get('SYNCER_BACKEND') == 'mem':
         raise ValueError('cannot unlock volatile state store')
 
     if settings.get('SYNCER_BACKEND') == 'fs': 
-        syncer_store_module = importlib.import_module('chainsyncer.store.fs')
-        syncer_store_class = getattr(syncer_store_module, 'SyncFsStore')
+        syncer_store_module = importlib.import_module('shep.store.file')
+        syncer_store_class = getattr(syncer_store_module, 'SimpleFileStoreFactory')
     elif settings.get('SYNCER_BACKEND') == 'rocksdb':
-        syncer_store_module = importlib.import_module('chainsyncer.store.rocksdb')
-        syncer_store_class = getattr(syncer_store_module, 'SyncRocksDbStore')
+        syncer_store_module = importlib.import_module('shep.store.rocksdb')
+        syncer_store_class = getattr(syncer_store_module, 'RocksdbStoreFactory')
     else:
-        syncer_store_module = importlib.import_module(settings.get('SYNCER_BACKEND'))
-        syncer_store_class = getattr(syncer_store_module, 'SyncStore')
+        raise NotImplementedError('cannot use backend: {}'.format(config.get('SYNCER_BACKEND')))
         
-    state_dir = os.path.join(config.get('_STATE_DIR'), settings.get('SYNCER_BACKEND'))
-    sync_path = os.path.join(config.get('_SESSION_ID'), 'sync', 'filter')
-    sync_store = syncer_store_class(state_dir, session_id=sync_path)
+    state_dir = config.get('_STATE_DIR')
 
-    logg.info('session is {}'.format(sync_store.session_id))
-
+    factory = syncer_store_class(state_dir)
+    base_state = PersistedState(factory.add, 0, check_alias=False)
+    state = FilterState(base_state, scan=True)
+    print(state)
 
 if __name__ == '__main__':
     main()
